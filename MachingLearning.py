8c04133239d3dba09d9f6e585f022c65608cc30a
soohyunkyung <113413847+soohyunkyung@users.noreply.github.com>
2024-04-16 23:07:10 +0900

    Update MachingLearning.py

--- MachingLearning.py
+++ MachingLearning.py
@@ -1,20 +1,30 @@
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
 from haversine import haversine
 
-def get_pharmacy(longitude, latitude, user_location)
-  kn=KNeighborsClassifier()
-  kn.fit(longitude, latitude) #드롭다운 된 경도, 위도를 가져와야 합니다.
-  kn.predict([user_location]) 
-  distances, indexes=kn.neighbors([user_location])
-  
-  name_results=[]
-  for i in indexes:
-    name_results.append(name[i])
-    
-  address_results=[] 
-  for i in indexes:
-    address_results.append(gu[i]+" "+road_name_adress[i]) #사용자 주변 5개의 약국 이름, 약국 주소를 출력합니다.
+def get_pharmacy(datas, user_latitude, user_longitude):
+    filtered_longitude=[]
+    filtered_latitude=[]
+    name_results=[]
+    address_results=[] 
+    distances_results=[]
+    for i in range(len(datas)):
+        filtered_longitude.append(float(datas[i]['longitude']))
+        filtered_latitude.append(float(datas[i]['latitude']))
+    filtered_longitude_array=np.array(filtered_longitude)
+    filtered_latitude_array=np.array(filtered_latitude)
 
-  distances_results=[]
-  for i in indexes:
-    distances_results.append(haversine(user_location,(longitude[i],latitude[i]))+"km")
+    kn=KNeighborsRegressor()
+
+    kn.fit(filtered_latitude_array.reshape(-1,1), filtered_longitude_array.reshape(-1,1)) #드롭다운 된 경도, 위도를 통해 학습합니다.
+    distances, indexes=kn.kneighbors([user_latitude])
+  
+    for i in indexes[0]:
+        name_results.append(datas[i]['name']) #사용자 주변 5개의 약국 이름을 저장합니다.
+    
+    for i in indexes[0]:
+        address_results.append(datas[i]['gu']+" "+datas[i]['road_name_address']) #사용자 주변 5개의 약국 주소를 저장합니다.
+
+    for i in indexes[0]:
+        distances_results.append(haversine((float(user_latitude[0]), float(user_longitude[0])),(float(datas[i]['latitude']),float(datas[i]['longitude'])))) #사용자로부터 가까운 5개 약국과의 거리를 저장합니다.
+
+    return name_results, address_results, distances_results

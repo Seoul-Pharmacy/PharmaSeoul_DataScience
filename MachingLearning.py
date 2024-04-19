2419c3407cb679169592c3b700ca8ad962686fab
soohyunkyung <113413847+soohyunkyung@users.noreply.github.com>
2024-04-19 22:16:28 +0900

    Update MachingLearning.py

--- MachingLearning.py
+++ MachingLearning.py
@@ -2,8 +2,10 @@
 
 from sklearn.neighbors import KNeighborsRegressor
 from haversine import haversine
+import numpy as np
 
 #datas, 유저의 위도&경도-> datas 내에서 사용자와 가까운 약국이 있는 5개의 딕셔너리와 해당 딕셔너리에 약국과의 거리를 추가하여 return.
+
 def get_pharmacy(datas, user_latitude, user_longitude):
     
     filtered_longitude=[]
@@ -15,12 +17,16 @@ def get_pharmacy(datas, user_latitude, user_longitude):
         filtered_latitude.append(float(datas[i]['latitude']))
     filtered_longitude_array=np.array(filtered_longitude)
     filtered_latitude_array=np.array(filtered_latitude)
+    
+    if (len(datas))>4:
+        kn=KNeighborsRegressor()
 
-    kn=KNeighborsRegressor()
+    elif(len(datas))<5:
+        kn=KNeighborsRegressor(n_neighbors=len(datas))
 
     kn.fit(filtered_latitude_array.reshape(-1,1), filtered_longitude_array.reshape(-1,1)) #드롭다운 된 경도, 위도를 통해 학습합니다.
     distances, indexes=kn.kneighbors([[user_latitude]])
-  
+    
     for i in indexes[0]:
         datas_results.append(datas[i])
     
